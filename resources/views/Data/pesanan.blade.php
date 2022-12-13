7f23aa1e7382febb6f38fda3dd5812d45e921783
Muhammad Rizki <muhammadrizkitkj123@gmail.com>
2022-12-13 22:15:04 +0800

    edit pesanan layout ;

--- resources/views/Data/pesanan.blade.php
+++ resources/views/Data/pesanan.blade.php
@@ -5,6 +5,7 @@
         <div class="card mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Data Pesanan</h6>
+               <a href="{{ route('cetak-pesanan') }}" target="blank" class="btn btn-primary">Cetak Data <i class="fa-solid fa-print"></i></a>
             </div>
             <div class="table-responsive p-3">
                 <table class="table align-items-center table-flush" id="dataTable">
