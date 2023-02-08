801c26b75837e5bf6e045b37cc2dd27d7393248c
Muhammad Rizki <muhammadrizkitkj123@gmail.com>
2023-02-08 21:19:00 +0800

    add ui new fitur delete all data

--- resources/views/Layouts/Base.blade.php
+++ resources/views/Layouts/Base.blade.php
@@ -8,6 +8,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     @include('Layouts.Styles')
 
 </head>
