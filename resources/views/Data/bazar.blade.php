7268f1f6a4fadebe30e8cd476dab4a10868c606c
Muhammad Rizki <muhammadrizkitkj123@gmail.com>
2023-02-08 21:18:13 +0800

    add cdn ajx

--- resources/views/Data/bazar.blade.php
+++ resources/views/Data/bazar.blade.php
@@ -23,7 +23,7 @@
                     </thead>
                     @php
                         $no = 1;
-                        
+
                     @endphp
 
                     @foreach ($data as $d)
@@ -56,7 +56,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="BazarModalLabel">Edit Data</h5>
+                    <h5 class="modal-title" id="BazarModalLabel">Tambah Data</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
